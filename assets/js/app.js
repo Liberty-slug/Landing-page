b986676f406f670266548721036c38f805fed96f
Liberty-slug <gbadamosilateef6@gmail.com>
2024-10-02 03:15:06 +0100

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
+// Get current year
+const spanYear = document.getElementById("presentYear");
+const currentYear = new Date().getFullYear();
+
+spanYear.innerHTML = currentYear;
+
+// Scroll to top visibility
+window.onscroll = function () {
+  const element = document.querySelector(".to-button");
+  if (window.scrollY > 50) {
+    element.classList.add("to-top");
+  } else {
+    element.classList.remove("to-top");
+  }
+};
+
+// Toggle menu
 const toggleIcon = document.getElementById("menu-icon");
 const navBar = document.querySelector(".mobile");
 
